ae73ca8d5caa1b64d5e0f50fc33d36f679e81ef5
Arek <arkadiusz.b.gawel@gmail.com>
2020-06-16 20:07:51 +0200

    objective transformation v2

--- model/init.py
+++ model/init.py
@@ -1,13 +1,9 @@
 from model.preprocess import Preprocess
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+
 
 path_to_file1 = './data.txt'
 path_to_file2 = './data2.txt'
 path_to_file3 = './data3.txt'
-
-
 
 
 a = Preprocess().read_text(path_to_file1)
@@ -15,28 +11,19 @@ b = Preprocess().read_text(path_to_file2)
 c = Preprocess().read_text(path_to_file3)
 
 a = Preprocess().stop_words_remove(a)
-print(a)
+b = Preprocess().stop_words_remove(b)
+c = Preprocess().stop_words_remove(c)
+
 
 
 print(Preprocess().jaro_similarity(a,c))
 print(Preprocess().seq_matcher_similarity(a,c))
 
 
-freq = Preprocess().count_words(b)
-freq = dict(freq.most_common(7))
-print(freq)
+freq = Preprocess().count_words(c)
 
+print(c)
 
+Preprocess().draw_histogram(freq, 5)
 
-labels, values = zip(*freq.items())
-
-indSort = np.argsort(values)[::-1]
-labels = np.array(labels)[indSort]
-values = np.array(values)[indSort]
-indexes = np.arange(len(labels))
-bar_width = 0.01
-
-plt.bar(indexes, values)
-plt.xticks(indexes + bar_width, labels)
-plt.show()
 
--- model/preprocess.py
+++ model/preprocess.py
@@ -2,6 +2,8 @@ from difflib import SequenceMatcher
 from collections import Counter
 import jellyfish
 import re
+import numpy as np
+import matplotlib.pyplot as plt
 
 stop_words = ['w','ach', 'aj', 'albo', 'bardzo', 'bez', 'bo', 'być', 'ci', 'cię', 'ciebie', 'co', 'czy', 'daleko', 'dla', 'dlaczego', 'dlatego', 'do', 'dobrze', 'dokąd', 'dość', 'dużo', 'dwa', 'dwaj', 'dwie', 'dwoje', 'dziś', 'dzisiaj', 'gdyby', 'gdzie', 'go', 'ich', 'ile', 'im', 'inny', 'ja', 'ją', 'jak', 'jakby', 'jaki', 'je', 'jeden', 'jedna', 'jedno', 'jego', 'jej', 'jemu', 'jeśli', 'jest', 'jestem', 'jeżeli', 'już', 'każdy', 'kiedy', 'kierunku', 'kto', 'ku', 'lub', 'ma', 'mają', 'mam', 'mi', 'mną', 'mnie', 'moi', 'mój', 'moja', 'moje', 'może', 'mu', 'my', 'na', 'nam', 'nami', 'nas', 'nasi', 'nasz', 'nasza', 'nasze', 'natychmiast', 'nią', 'nic', 'nich', 'nie', 'niego', 'niej', 'niemu', 'nigdy', 'nim', 'nimi', 'niż', 'obok', 'od', 'około', 'on', 'ona', 'one', 'oni', 'ono', 'owszem', 'po', 'pod', 'ponieważ', 'przed', 'przedtem', 'są', 'sam', 'sama', 'się', 'skąd', 'tak', 'taki', 'tam', 'ten', 'to', 'tobą', 'tobie', 'tu', 'tutaj', 'twoi', 'twój', 'twoja', 'twoje', 'ty', 'wam', 'wami', 'was', 'wasi', 'wasz', 'wasza', 'wasze', 'we', 'więc', 'wszystko', 'wtedy', 'wy', 'żaden', 'zawsze', 'że']
 
@@ -33,4 +35,17 @@ class Preprocess:
 
     def seq_matcher_similarity (self, a, b):
         return SequenceMatcher(None, a,  b).ratio()
+
+
+    def draw_histogram(self, freq, n_elements):
+        self.freq = dict(freq.most_common(n_elements))
+        labels, values = zip(*self.freq.items())
+        sorted_indicator = np.argsort(values)[::-1]
+        labels = np.array(labels)[sorted_indicator]
+        values = np.array(values)[sorted_indicator]
+        prepared = np.arange(len(labels))
+
+        plt.bar(prepared, values)
+        plt.xticks(prepared, labels)
+        plt.show()
 
